4a80e8b3548a19ea32d52ebe3d96df21f1039973
Sachin Kumar <sachin1407.kumar@gmail.com>
2023-07-20 10:24:50 +0530

    search button added and working

--- src/Body.js
+++ src/Body.js
@@ -1,8 +1,23 @@
 import { resList } from "./constant";
 import Restaurentcard from "./restaurant_card";
+import { useState } from "react";
+ 
+
 
 const Body=function (){
+    // let searchval="kfc";
+    const [searchval, setsearchval]=useState("");
     return (
+        <>
+        <div className="search-container">
+           <input type="text" className="search-input" placeholder="search" 
+           value={searchval} 
+            onChange={(e)=>{
+                setsearchval(e.target.value);
+            }}
+           />
+           <button className="search-btn">search</button>
+        </div>
       <div className="body-comp">
       {
       resList.map((restaurent)=>{
@@ -12,6 +27,7 @@ const Body=function (){
      
      
       </div>
+      </>
     )
   }
 
