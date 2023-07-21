b92badd95f9395b36fbb8d456fa8366715cdd45c
Sachin Kumar <sachin1407.kumar@gmail.com>
2023-07-21 11:53:02 +0530

    search is working but .include is not working

--- src/Body.js
+++ src/Body.js
@@ -1,26 +1,56 @@
 import { resList } from "./constant";
 import Restaurentcard from "./restaurant_card";
 import { useState } from "react";
+
+function filterdata(searchval, Reslist){
+  const newdata=Reslist.filter((Res)=>{
+   
+    const stringname=Res.data.data.name;
+  //  return  stringname.indexOf(searchval) !== -1
+       return stringname===searchval
+         
+  })
+  return newdata;
+}
+
+
  
 
-
 const Body=function (){
+  
+  
     // let searchval="kfc";
-    const [searchval, setsearchval]=useState("");
-    return (
+    // this is way of creating local state variable 
+   let [searchval, setsearchval]=useState(""); 
+   let [Reslist, setReslist]=useState(resList);
+   
+   
+   return (
         <>
-        <div className="search-container">
-           <input type="text" className="search-input" placeholder="search" 
-           value={searchval} 
-            onChange={(e)=>{
-                setsearchval(e.target.value);
-            }}
+     
+        <div>
+          <input type="text" className="search-container" placeholder="search"
+          value={searchval}
+          onChange={(e)=>{
+            setsearchval(e.target.value);
+          // searchval=e.target.value;  it won't work 
+           }}
            />
-           <button className="search-btn">search</button>
+          <button className="search-btn" 
+          onClick={()=>{
+            const Data=filterdata(searchval, Reslist);
+            setReslist(Data);
+          }
+                   
+          }
+          
+          >search
+          </button>
         </div>
+        
       <div className="body-comp">
       {
-      resList.map((restaurent)=>{
+      Reslist.map((restaurent)=>{
         return <Restaurentcard  {...restaurent.data.data} key={restaurent.data.data.id}/>
       })};
     
